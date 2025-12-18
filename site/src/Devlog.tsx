5f71716cd09e9ff4a4998bece62c88189ea6df3a
K4LL <k4ll.569lk@gmail.com>
2025-12-18 18:39:34 -0300

    update devlog

--- site/src/Devlog.tsx
+++ site/src/Devlog.tsx
@@ -13,8 +13,28 @@ export function Devlog() {
 
           <ul className="section devlog-content">
             <li>
-              <h2>11/12/2025 or 11/12/2025 if you are on US 🦅🦅🦅</h2>
+              <h2>2025-12-18</h2>
+              <h4>09:17 -</h4>
               <p>
+                Now I finally finished the site. It should be funny, but I'm too lazy and need to fix the engine itself. So, no time
+                for the site.
+              </p>
+              <h4>16:31 -</h4>
+              <p>
+                So, now I will get back to work on the engine. Basically, everything is broken. The renderer in the newer branches
+                doesn't render anything, the old one is not broken, but has some "specifications" that I really need to change.
+                Also, I think there are lots of bugs and memory leaks that I fixed in the newer versions.
+                I'm thinking about getting the newer branches and just throw the new renderer on the old renderer, but it may not work, so
+                probably I will need to fix everything by hand.
+                Also, I will try to port a part of the engine to Rust, but I don't really know if it will be a good idea. It could maybe
+                help with safety, debugging, and if i try to make multithreading, it would also help with performance, even tho it is not
+                the main goal right now.
+              </p>
+            </li>
+            <li>
+              <h2>2025-11-12</h2>
+              <p>
+                <h4>??:?? -</h4>
                 I just finished the landing page and created the devlog. I need to get back to work on the engine itself.
               </p>
             </li>
